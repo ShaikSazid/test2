7d9fe786fc72a6c5d8789e03e2ce7f716c0ae74e
ShaikSazid <Shaiksazid7386@gmail.com>
2026-04-10 16:20:14 +0530

    Testing CI/CD pipeline

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
     res.send("Hey how are you ?");
 });
 
+app.get("/name", (req, res) => {
+    res.send("Hey what's your name .. never mind you are a fucking cunt!!");
+})
+
 app.listen(port, () => {
     console.log(`Server running on the port ${port}`);
 });
